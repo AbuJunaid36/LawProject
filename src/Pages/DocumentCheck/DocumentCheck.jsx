58273507e2e06f1f72a9bc5f6395a4f2a7d14873
AbuJunaid36 <abujunaid36@gmail.com>
2025-02-20 05:02:08 +0600

    update

--- src/Pages/DocumentCheck/DocumentCheck.jsx
+++ src/Pages/DocumentCheck/DocumentCheck.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const DocumentCheck = () => {
   return (
-    <div className="w-[90%] md:w-[80%] mx-auto mb-20 md:mb-0 flex items-center justify-center p-4">
-      <div className="bg-white p-8 rounded-xl shadow-2xl w-full max-w-4xl">
+    <div className=" md:w-[80%] mx-auto mb-20 md:mb-0 flex items-center justify-center p-4">
+      <div className="bg-white p-8 rounded-xl shadow-2xl w-full">
         <h1 className="text-3xl font-bold text-center text-blue-800 mb-8">
           DocumentCheck
         </h1>
         <form className="space-y-6">
           {/* Dropdowns */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {["বিভাগ", "জেলা", "জরিপ", "থানা", "মৌজা"].map((label, index) => (
               <div key={index}>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
                   {label}:
                 </label>
-                <select className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all">
+                <select className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all">
                   <option>নির্বাচন করুণ</option>
                   <option value="1">{label} ১</option>
                   <option value="2">{label} ২</option>
@@ -26,7 +26,7 @@ const DocumentCheck = () => {
           </div>
 
           {/* Text Inputs */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {[
               "বর্তমান খতিয়ান",
               "মালিক",
@@ -48,7 +48,7 @@ const DocumentCheck = () => {
                 </label>
                 <input
                   type="text"
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
                 />
               </div>
             ))}
